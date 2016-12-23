da21b81aaa49ccf0845c26b41fcc8b414c91d62b
ruslanprikhdk <ruslanprikhdk@gmail.com>
2016-12-23 13:14:01 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-﻿
+﻿var obj = {
+  title: 'Тест по программированию',
+  question1:'1. Вопрос №1',
+  set:{
+    answer1:'Вариант ответа №1',
+    answer2:'Вариант ответа №2',
+    answer3:'Вариант ответа №3'},
+  question2:'2. Вопрос №2',
+  set:{
+    answer1:'Вариант ответа №1',
+    answer2:'Вариант ответа №2',
+    answer3:'Вариант ответа №3'},
+  question3:'3. Вопрос №3',
+  set:{
+    answer1:'Вариант ответа №1',
+    answer2:'Вариант ответа №2',
+    answer3:'Вариант ответа №3'},
+  button:'Проверить мои результаты' }
+
+var wrapper = document.querySelector('.wrapper');
+
 function createH1(text) {
   var h1 = document.createElement('h1');
   h1.appendChild(document.createTextNode(text));
@@ -11,56 +31,58 @@ function createDiv(text) {
   return div;  
 }  
 
-function createInput(text) {
+function createForm(text1, text2, text3) {
+  var form = document.createElement('form');  
+  var label = document.createElement('label');
   var input = document.createElement('input');
   input.type = 'checkbox';
-  return input;  
-}
-
-function createLabel(text) {
-  var label = document.createElement('p');
-  label.appendChild(document.createTextNode(text));
-  return label;
+  input.name = 'name';
+  label.appendChild(input);
+  label.appendChild(document.createTextNode(text1));
+  form.appendChild(label);
+  var label = document.createElement('label');
+  var input = document.createElement('input');
+  input.type = 'checkbox';
+  input.name = 'name';
+  label.appendChild(input);
+  label.appendChild(document.createTextNode(text2));
+  form.appendChild(label);
+  var label = document.createElement('label');
+  var input = document.createElement('input');
+  input.type = 'checkbox';
+  input.name = 'name';
+  label.appendChild(input);
+  label.appendChild(document.createTextNode(text3));
+  form.appendChild(label);
+  return form;
 }
 
 function createButton(text) {
+  var form = document.createElement('form');
   var button = document.createElement('input');
   button.type = 'button';
-  button.value = 'Проверить мои результаты';
-  return button;
+  button.name = 'name2';
+  button.value = (text);
+  form.appendChild(button);
+  return form;
 }
 
 var elements = [
-  createH1('Тест по программированию'),
-  createDiv('1. Вопрос №1'),
-  createInput(),
-  createLabel('Вариант ответа №1'),
-  createInput(),
-  createLabel('Вариант ответа №2'),
-  createInput(),
-  createLabel('Вариант ответа №3'),
-  createDiv('2. Вопрос №2'),
-  createInput(),
-  createLabel('Вариант ответа №1'),
-  createInput(),
-  createLabel('Вариант ответа №2'),
-  createInput(),
-  createLabel('Вариант ответа №3'),
-  createDiv('3. Вопрос №3'),
-  createInput(),
-  createLabel('Вариант ответа №1'),
-  createInput(),
-  createLabel('Вариант ответа №2'),
-  createInput(),
-  createLabel('Вариант ответа №3'),
-  createButton()
+  createH1(obj.title),
+  createDiv(obj.question1),
+  createForm(obj.set.answer1, obj.set.answer2, obj.set.answer3),
+  createDiv(obj.question2),
+  createForm(obj.set.answer1, obj.set.answer2, obj.set.answer3),
+  createDiv(obj.question3),
+  createForm(obj.set.answer1, obj.set.answer2, obj.set.answer3),
+  createButton(obj.button)
 ];
 
-var docFrag = document.createDocumentFragment();
-for(var i = 0; i < elements.length; i++) {
-  docFrag.appendChild(elements[i]);
+function DOM () {
+  for(var i = 0; i < elements.length; i++) {
+  wrapper.appendChild(elements[i]);
+  }
 }
 
-document.body.appendChild(docFrag);
-
+DOM ()
 
